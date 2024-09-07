1a1ef3454a910bff0a92c23b0658d063c9a1874d
SonsGLi <sonsgli2841475634@gmail.com>
2024-09-08 01:07:12 +0800

    zz

---
+++ init.lua
@@ -0,0 +1,8 @@
+require("core.options")
+require("core.keymaps")
+require("plugins.plugins-setup")
+
+
+
+
+
---
+++ lua/core/keymaps.lua
@@ -0,0 +1,19 @@
+vim.g.mapleader = " "    --主键
+
+local keymap = vim.keymap
+
+--正常模式
+
+--窗口分割
+keymap.set("n","<leader>sv","<C-w>v>") -- 水平新增
+keymap.set("n","<leader>sh","<C-w>s")  --垂直新增
+
+
+
+
+
+
+
+
+
+
